35afb4e70f5c0ddfb96299b9afa7f8115d9e4a7b
oppong <oppong@oppongs-MacBook-Pro.local>
2018-07-21 14:40:27 -0400

    first commit

---
+++ adventure.py
@@ -0,0 +1,40 @@
+#adventure text based game
+
+def position_0():
+    print ("<---<---<---<---")
+    print("\t\t^")
+    print("\t\t|")
+    print("\t\t|")
+    print ("X--->--->")
+    print("\t|\t^")
+    print("\t|\t|")
+    print("\tv\t|")
+    print("\t|\t^")
+    print("\t|\t|")
+    print("\tv\t|")
+    print("\t--->--->")
+
+def position_1():
+    print ("<---<---<---<---")
+    print("\t\t^")
+    print("\t\t|")
+    print("\t\t|")
+    print ("--->--->M")
+    print("\t|\t^")
+    print("\t|\t|")
+    print("\tv\t|")
+    print("\t|\t^")
+    print("\t|\t|")
+    print("\tv\t|")
+    print("\t--->--->")
+
+print("\n----- Heyyy, welcome to the Oppong Adventure Game -----\n")
+print("\n----- The objective of this game is to go through the maze looking for something-----\n")
+
+user_char = raw_input("What is the First Letter of your First Name,")
+
+
+
+
+
+
