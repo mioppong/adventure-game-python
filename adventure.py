153bf48cec93733c3985f7c925cda4d5e772a675
oppong <oppong@oppongs-MacBook-Pro.local>
2018-07-23 18:45:23 -0400

    moving through levels up to level 5

--- adventure.py
+++ adventure.py
@@ -1,4 +1,7 @@
 #adventure text based game
+#adventure text based game
+def bad_move():
+    print "\nLOL that seems like a bad move\n"
 
 def clear_terminal():
     print(chr(27) + "[2J")
@@ -7,7 +10,6 @@ def press_to_continue():
     raw_input("\npress enter to continue...")
 
 def position_0(user_char):
-    clear_terminal()
     print "<---<---<---<---"
     print "\t\t^"
     print "\t\t|"
@@ -22,7 +24,6 @@ def position_0(user_char):
     print "\t--->--->"
 
 def position_1(user_char):
-    clear_terminal()
     print "<---<---<---<---"
     print "\t\t^"
     print "\t\t|"
@@ -36,7 +37,89 @@ def position_1(user_char):
     print "\tv\t|"
     print "\t--->--->"
 
-def get_char():
+def position_2(user_char):
+    print "<---<---<---<---"
+    print "\t\t^"
+    print "\t\t|"
+    print "\t\t|"
+    print "--->-->"
+    print "\t|\t^"
+    print "\t|\t|"
+    print "\tv\t|"
+    print "\t|\t^"
+    print "\t|\t|"
+    print "\tv\t|"
+    print "\t",user_char
+    print "\t--->--->"
+
+def position_3(user_char):
+    print "<---<---<---<---"
+    print "\t\t^"
+    print "\t\t|"
+    print "\t\t|"
+    print "--->-->"
+    print "\t|\t^"
+    print "\t|\t|"
+    print "\tv\t|"
+    print "\t|\t^"
+    print "\t|\t|"
+    print "\tv\t|"
+    print "\t--->--->", user_char
+
+def position_4(user_char):
+    print "<---<---<---<---"
+    print "\t\t^"
+    print "\t\t|"
+    print "\t\t|"
+    print "--->-->\t\t",user_char
+    print "\t|\t^"
+    print "\t|\t|"
+    print "\tv\t|"
+    print "\t|\t^"
+    print "\t|\t|"
+    print "\tv\t|"
+    print "\t--->--->"
+def position_5(user_char):
+    print "<---<--<---<---", user_char
+    print "\t\t^"
+    print "\t\t|"
+    print "\t\t|"
+    print "--->-->"
+    print "\t|\t^"
+    print "\t|\t|"
+    print "\tv\t|"
+    print "\t|\t^"
+    print "\t|\t|"
+    print "\tv\t|"
+    print "\t--->--->"
+def position_6(user_char):
+    print "<--<---",user_char,"<---<--"
+    print "\t\t^"
+    print "\t\t|"
+    print "\t\t|"
+    print "--->-->"
+    print "\t|\t^"
+    print "\t|\t|"
+    print "\tv\t|"
+    print "\t|\t^"
+    print "\t|\t|"
+    print "\tv\t|"
+    print "\t--->--->"
+def position_7(user_char):
+    print user_char,"<---<---<---<---"
+    print "\t\t^"
+    print "\t\t|"
+    print "\t\t|"
+    print "--->-->"
+    print "\t|\t^"
+    print "\t|\t|"
+    print "\tv\t|"
+    print "\t|\t^"
+    print "\t|\t|"
+    print "\tv\t|"
+    print "\t--->--->"
+
+def get_char(): #get user player
     user_char = raw_input("Pick a single char to use as your player.\neg. 'A','$','*', etc\npick: ")
 
     if len(user_char)!= 1 :
@@ -45,7 +128,7 @@ def get_char():
         return user_char
 
 def start_game():
-    user_ready = raw_input("are you ready to start now?, Y, N:")
+    user_ready = raw_input("are you ready to start now?, Y, N: ")
     if (user_ready == 'Y'):
         return 1 #game can now begin
     else:
@@ -54,7 +137,15 @@ def start_game():
 def possible_moves():
     print("\npossible moves:\nU for up\nD for down\nL for left\nR for right")
 
+def move_up_one_level(current_level):
+    return (current_level+1)
+
+def get_move_from_user():
+    return raw_input("choice: ")
+ 
 # --CODE STARTS HEREEEE-- --CODE STARTS HEREEEE-- --CODE STARTS HEREEEE-- --CODE STARTS HEREEEE-- --CODE STARTS HEREEEE--
+clear_terminal()
+
 print("\n----- Heyyy, welcome to the Oppong Adventure Game -----\n")
 print("-----Go through the maze looking for and just have fun -----\n")
 
@@ -63,23 +154,39 @@ clear_terminal()
 user_char = get_char() # GETS USER CHAR FOR THE MAP
 clear_terminal()
 
-move_made = ""
+current_level = 0 #keep track on user position
 
-if (start_game() == 1):
-    position_0 (user_char)
+if (start_game() == 1 and current_level == 0):
     possible_moves()
-    move_made = raw_input("choice: ")
+    position_0 (user_char)
+    
+    keep_playing = True
+    
+while True:
 
-    if move_made is 'U' :
-        print "Gang shit"
+    if get_move_from_user() == 'R' and current_level == 0:
+        current_level = current_level + 1
+        position_1(user_char)
+    if get_move_from_user() == 'D' and current_level == 1:
+        current_level = current_level + 1        
+        position_2(user_char)
+    if get_move_from_user() == 'R' and current_level ==2:
+        current_level = current_level + 1
+        position_3(user_char)
+    if get_move_from_user() == 'U' and current_level == 3:
+        current_level = current_level +1
+        position_4(user_char)
+    if get_move_from_user() == 'U' and current_level == 4:
+        current_level = current_level + 1
+        position_5(user_char)
+    
+
+
+        
 
 
 
     
-
-
-
-
 
 
 
